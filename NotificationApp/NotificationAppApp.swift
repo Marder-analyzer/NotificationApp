e47639db4485e7031adf0cac43456d3f121fd59c
Marder-analyzer <mehmetcan.arslan.engineer@gmail.com>
2025-12-03 17:23:49 +0300

    Initial Commit

---
+++ NotificationApp/NotificationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationAppApp.swift
+//  NotificationApp
+//
+//  Created by Rumeysa Tokur on 3.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct NotificationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
